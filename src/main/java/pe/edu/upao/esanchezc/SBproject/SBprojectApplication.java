48944211481beefc73651c7dd505d50a13d83e28
DianCarolinee <Dianacs78@hotmail.com>
2024-10-31 19:40:13 -0500

    first commit

---
+++ src/main/java/pe/edu/upao/esanchezc/SBproject/SBprojectApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.upao.esanchezc.SBproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SBprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SBprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/upao/esanchezc/SBproject/SBprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.upao.esanchezc.SBproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SBprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
